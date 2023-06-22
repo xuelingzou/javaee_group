c93968deea33b20182f8edacb63ec406b87ce9c4
wfl <2571844546@qq.com>
2023-06-22 20:20:32 +0800

    修改目录

--- microservice_server/NetworkFreightSys/user/src/main/java/com/example/auto_warehouse/circuitBreaker/DeptController.java
+++ microservice_server/NetworkFreightSys/user/src/main/java/com/example/auto_warehouse/circuitBreaker/DeptController.java
@@ -1,4 +1,4 @@
-package com.example.auto_warehouse.hystrix;
+package com.example.auto_warehouse.circuitBreaker;
 
 
 
--- microservice_server/NetworkFreightSys/user/src/main/java/com/example/auto_warehouse/circuitBreaker/DeptServiceImpl.java
+++ microservice_server/NetworkFreightSys/user/src/main/java/com/example/auto_warehouse/circuitBreaker/DeptServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.auto_warehouse.hystrix;
+package com.example.auto_warehouse.circuitBreaker;
 
 import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
