2f86c1b4e4ee692f305071fe4341086681edb57a
wfl <2571844546@qq.com>
2023-06-22 22:47:57 +0800

    增加熔断器配置，并在设计文档上增加配置说明

---
+++ microservice_server/NetworkFreightSys/user/src/main/java/com/example/auto_warehouse/circuitBreaker/DeptService.java
@@ -0,0 +1,13 @@
+package com.example.auto_warehouse.circuitBreaker;
+
+public interface DeptService {
+
+        // hystrix 熔断器示例 ok
+        public String deptInfo_Ok(Integer id);
+
+        //hystrix 熔断器超时案例
+        public String deptInfo_Timeout(Integer id);
+
+        // Hystrix 熔断机制案例
+        public String deptCircuitBreaker(Integer id);
+}
