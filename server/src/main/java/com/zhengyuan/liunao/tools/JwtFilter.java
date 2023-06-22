a8777578a1c8b1e7688d525b2544832ba6c52a4c
zjq <726532751@qq.com>
2023-06-22 18:39:03 +0800

    spring security和oauth2

---
+++ server/src/main/java/com/zhengyuan/liunao/tools/JwtFilter.java
@@ -0,0 +1,88 @@
+package com.zhengyuan.liunao.tools;
+
+import com.auth0.jwt.interfaces.Claim;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+
+//@WebFilter(filterName = "JwtFilter", urlPatterns = "/*")
+@WebFilter(filterName = "JwtFilter", urlPatterns = "/v1/users/login")
+public class JwtFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("JwtFilter---->init()");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) req;
+        final HttpServletResponse response = (HttpServletResponse) res;
+
+        //设置跨域请求
+        //response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8083");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,HEAD,PUT,PATCH");
+        response.setHeader("Access-Control-Max-Age", "36000");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization,authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        //        属性来校验自定义请求头,在请求头header中设置了了自定义的token字段，所以跨域请求就认为这是一个复杂的请求，他就会先进行预校验，也就是我们说的Options请求，
+        //        等Options请求成功之后它才会进行post或者get请求，所以在发送Options请求的时候要校验请求头
+        response.setHeader("Access-Control-Allow-Headers","token");
+//        response.setHeader("Access-Control-Allow-Headers","token,content-type");
+
+        //获取请求URI
+        String requestURI = request.getRequestURI();
+
+        //让行
+        /*if(requestURI=="/cargo/show_notInput"||requestURI=="/cargo/show_notOutput"){
+            chain.doFilter(request, response);
+            System.out.println("66666666666666"+requestURI);
+        }*/
+
+        response.setCharacterEncoding("UTF-8");
+        /// 从 http 请求头中获取 header里的token
+        final String token = request.getHeader("token");
+        System.out.println("token是:"+token);
+        System.out.println("method"+request.getMethod());
+
+
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            chain.doFilter(request, response);
+            System.out.println("111111111111111111111");
+        }
+        else {
+            // Except OPTIONS, other request should be checked by JWT
+            if (token == null) {
+                response.getWriter().write("No token!");
+                return;
+            }
+
+            Map<String, Claim> userData = JwtUtil.verifyToken(token);
+            if (userData == null) {
+                response.getWriter().write("token不合法！");
+                System.out.println("4444444444444444444444444");
+                return;
+            }
+            String id = userData.get("id").asString();
+//            String userName = userData.get("userName").asString();
+            String password= userData.get("password").asString();
+            //拦截器 拿到用户信息，放到request中
+            request.setAttribute("id", id);
+//            request.setAttribute("userName", userName);
+            request.setAttribute("password", password);
+            chain.doFilter(req, res);
+            System.out.println("555555555555555555555"+id);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ server/src/main/java/com/zhengyuan/liunao/tools/JwtUtil.java
@@ -0,0 +1,83 @@
+package com.zhengyuan.liunao.tools;
+
+import com.zhengyuan.liunao.entity.Admin;
+import com.zhengyuan.liunao.entity.Client;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class JwtUtil {
+    /**
+     * 密钥(签名)
+     */
+    private static final String SECRET = "my_secret";
+
+    /**
+     * 过期时间
+     **/
+    private static final long EXPIRATION = 1800L;//单位为秒
+//    private static final long EXPIRATION = 10L;
+
+    /**
+     * 生成用户token,设置token超时时间
+     */
+    public static String createToken(Client client) {
+        //过期时间
+        Date expireDate = new Date(System.currentTimeMillis() + EXPIRATION * 1000);
+        Map<String, Object> map = new HashMap<>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+        String token = JWT.create()
+                .withHeader(map)// 添加头部
+                //可以将基本信息放到claims中
+                .withClaim("id", client.getCeid())//userId
+                .withClaim("password", client.getPsw())//password
+                .withExpiresAt(expireDate) //超时设置,设置过期的日期
+                .withIssuedAt(new Date()) //签发时间
+                .sign(Algorithm.HMAC256(SECRET)); //SECRET加密
+        return token;
+    }
+
+    public static String createToken(Admin admin) {
+        //过期时间
+        Date expireDate = new Date(System.currentTimeMillis() + EXPIRATION * 1000);
+        Map<String, Object> map = new HashMap<>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+        String token = JWT.create()
+                .withHeader(map)// 添加头部
+                //可以将基本信息放到claims中
+                .withClaim("id", admin.getAccount())//userId
+                .withClaim("password", admin.getPsw())//password
+                .withExpiresAt(expireDate) //超时设置,设置过期的日期
+                .withIssuedAt(new Date()) //签发时间
+                .sign(Algorithm.HMAC256(SECRET)); //SECRET加密
+        return token;
+    }
+
+    /**
+     * 校验token并解析token
+     */
+    public static Map<String, Claim> verifyToken(String token) {
+        DecodedJWT jwt = null;
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+            jwt = verifier.verify(token);
+
+            //decodedJWT.getClaim("属性").asString()  获取负载中的属性值
+
+        } catch (Exception e) {
+            System.out.println("token解码异常");
+            //解码异常则抛出异常
+            return null;
+        }
+        return jwt.getClaims();
+    }
+}
